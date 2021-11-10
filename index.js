c2c2bc53aaba0acc8dfa68c7e2818d589767d8a6
杉本　諒 <wasp2123321@gmail.com>
2021-11-10 22:53:17 +0900

    Insert last node

--- index.js
+++ index.js
@@ -14,9 +14,27 @@ class LinkedList {
   // Insert first node
   insertFirst(data) {
     this.head = new Node(data, this.head)
+    this.size++;
   }
 
   // Insert last node
+  insertLast(data) {
+    let node = new Node(data)
+    let current
+
+    // if empty, make head
+    if (!this.head) {
+      this.head = node
+    } else {
+      current = this.head
+      while (current.next) {
+        current = current.next
+      }
+
+      current.next = node
+    }
+    this.size++;
+  }
 
   // Insert at index
 
@@ -42,5 +60,6 @@ const ll = new LinkedList()
 ll.insertFirst(100)
 ll.insertFirst(200)
 ll.insertFirst(300)
+ll.insertLast(400)
 
 ll.printListData()
