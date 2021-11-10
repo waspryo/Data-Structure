ec27f0d333e39793a3b88bddf15758995a7ddd3b
杉本　諒 <wasp2123321@gmail.com>
2021-11-10 22:34:43 +0900

    make todo

---
+++ index.js
@@ -0,0 +1,28 @@
+class Node {
+  constructor(data, next = null) {
+    this.data = data
+    this.next = next
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null
+    this.size = 0
+  }
+  // Insert first node
+
+  // Insert last node
+
+  // Insert at index
+
+  // Get at index
+
+  // Remove at index
+
+  // Clear list
+
+  // Print list data
+
+
+}
