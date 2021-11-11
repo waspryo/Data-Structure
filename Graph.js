be9d5c061b33655bd6d3c43816feba9332704ad5
杉本　諒 <wasp2123321@gmail.com>
2021-11-11 10:54:34 +0900

    Graph

---
+++ Graph.js
@@ -0,0 +1,34 @@
+const airports = "PHX BKK OKC JFK LAX MEX EZE HEL LOS LAP LIM".split(" ");
+
+const routes = [
+  ["PXH", "LAX"],
+  ["PXH", "JFK"],
+  ["JFK", "OKC"],
+  ["JFK", "HEL"],
+  ["JFK", "LOS"],
+  ["MEX", "LAX"],
+  ["MEX", "BKK"],
+  ["MEX", "LIM"],
+  ["MEX", "EZE"],
+  ["LIM", "BKK"],
+];
+
+// The graph
+const adjacencyList = new Map();
+
+// Add node
+function addNode(airport) {
+  adjacencyList.set(airport, []);
+}
+
+// Add edge, undirected
+function addEdge(origin, destination) {
+  adjacencyList.get(origin).push(destination);
+  adjacencyList.get(destination).push(origin);
+}
+
+// Create the graph
+airports.forEach(addNode);
+routes.forEach((route) => addEdge(...route));
+
+console.log(adjacencyList);
